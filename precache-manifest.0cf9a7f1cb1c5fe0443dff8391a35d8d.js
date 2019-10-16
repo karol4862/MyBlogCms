c0e33986ac9727e20ca6728ec1f98989307d6534
Karol Makara <karol_makara17@wp.pl>
2019-10-16 16:55:51 +0200

    Updates

---
+++ precache-manifest.0cf9a7f1cb1c5fe0443dff8391a35d8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84a152c2b3f3c826a601486ba14fbdd5",
+    "url": "/MyBlogCms/index.html"
+  },
+  {
+    "revision": "c204f58907513427adb7",
+    "url": "/MyBlogCms/static/css/main.d678d7f5.chunk.css"
+  },
+  {
+    "revision": "434fcb0fa6eac7d27341",
+    "url": "/MyBlogCms/static/js/2.e0d0aa74.chunk.js"
+  },
+  {
+    "revision": "c204f58907513427adb7",
+    "url": "/MyBlogCms/static/js/main.f7551ba6.chunk.js"
+  },
+  {
+    "revision": "820f7cc088d23e93818a",
+    "url": "/MyBlogCms/static/js/runtime~main.b54587cf.js"
+  },
+  {
+    "revision": "10450828f91da91d8eda58204387c0ea",
+    "url": "/MyBlogCms/static/media/code-1076536_1920.10450828.jpg"
+  },
+  {
+    "revision": "1251f54f300cf1cd12d8e7a0141d97a1",
+    "url": "/MyBlogCms/static/media/work.1251f54f.jpg"
+  }
+]);
